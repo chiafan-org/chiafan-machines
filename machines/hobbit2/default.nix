c6570c38ff8d7b230470da0f5786d7f024c13b05
Break Yang <breakds@gmail.com>
2021-06-06 14:04:05 -0700

    Open port for iscsi target on hobbit2

--- machines/hobbit2/default.nix
+++ machines/hobbit2/default.nix
@@ -22,6 +22,7 @@
     services.target = {
       enable = true;
     };
+    networking.firewall.allowedTCPPorts = [ 3260 ];
 
     # This value determines the NixOS release from which the default
     # settings for stateful data, like file locations and database versions
